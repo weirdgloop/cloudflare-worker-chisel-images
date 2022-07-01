3c71c24a2b575e627eeab4fb4d1cdd3089196631
Gaz L <gazllloyd@gmail.com>
2022-07-01 18:29:05 +0100

    single bucket access

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,15 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+ * R2 access worker
+ * allows GET access to items in the chisel-images bucket
+ * 405s if not GET
+ * 404s if no such file
+ * 
+ * https://developers.cloudflare.com/workers/
  */
 
 export interface Env {
-    // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-    // MY_KV_NAMESPACE: KVNamespace;
-    //
-    // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-    // MY_DURABLE_OBJECT: DurableObjectNamespace;
-    //
-    // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-    TEST_BUCKET: R2Bucket;
-}
-
-const getBucketNamed = (env:Env, name:string):R2Bucket|null => {
-    switch (name) {
-        case 'test-bucket':
-            return env.TEST_BUCKET;
-    }
-    return null;
+    // binding to R2. https://developers.cloudflare.com/workers/runtime-apis/r2/
+    CHISEL_IMAGES: R2Bucket;
 }
 
 export default {
@@ -43,19 +28,16 @@ export default {
         }
         const url = new URL(request.url);
         /*
-        if the url is 'https://r2.weirdgloop.org/test-bucket/imgs/wowee.png'
-        pathname is '/test-bucket/imgs/wowee.png'
-        split(/, 3) is ['', 'test-bucket', 'imgs/wowee.png']
+        if the url is 'https://r2.weirdgloop.org/imgs/wowee.png'
+        pathname is '/imgs/wowee.png'
+        .slice(1) is 'imgs/wowee.png'
         */
-        let split_url = url.pathname.split('/', 3);
-        let bucket_name = split_url[1];
-        let object_key = split_url[2];
-        let bucket = getBucketNamed(env, bucket_name);
+        let object_key = url.pathname.slice(1);
 
-        if (bucket === null) {
+        if (env.CHISEL_IMAGES === null) {
             return new Response('Bucket/Object Not Found', {status:404});
         }
-        let obj = await bucket.get(object_key);
+        let obj = await env.CHISEL_IMAGES.get(object_key);
         if (obj === null) {
             return new Response('Bucket/Object Not Found', {status:404});
         }
